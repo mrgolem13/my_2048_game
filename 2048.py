ffdd256b35f06df7d7fec29027950da5d13656a2
mrgolem13-web_DB_trial <021128rewq@gmail.com>
2026-05-31 14:14:42 +0900

    Migration: SQLite에서 외부 Supabase 영구 저장 DB로 교체

--- 2048.py
+++ 2048.py
@@ -252,6 +252,8 @@ def admin_delete_score(score_id):
     return redirect(url_for('admin_dashboard'))
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+app.run(debug=True)
